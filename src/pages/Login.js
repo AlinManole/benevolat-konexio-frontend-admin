7d7e21d0e5f6a1db7eae8fc9d2070b425f19d635
Alin Manole <alin.manole@yahoo.com>
2022-02-01 16:28:43 +0100

    create Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,42 @@
 import React from 'react';
+// import { useFormik, formik } from 'formik';
+import styled from "styled-components"
 
+
+const MainPage = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+const Form = styled.form`
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    width: 500px;
+`;
 const Login = () => {
     return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
+        <MainPage>
+            <img src="./src/images/konexio.png" alt="Konexio Logo"></img>
+            <Form>
+                <h2>Connectez-vous à l’espace admin</h2>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder='Email'
+                />
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder='Password'
+                />
+
+                <button type="submit">Se connecter</button>
+            </Form>
+        </MainPage>
     );
 };
 
