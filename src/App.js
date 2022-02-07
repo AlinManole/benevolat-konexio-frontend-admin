027113c75e17e53d7a6c0d63242a9fca09874793
Alin Manole <alin.manole@yahoo.com>
2022-02-07 14:21:17 +0100

    update delet FAQ from Admin

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Login from "./pages/Login";
 import NotFound from "./pages/NotFound.js";
 import Benevoles from "./pages/Benevoles";
 import Calendrier from "./pages/Calendrier";
-import Faq from "./pages/Faq";
 import Messagerie from "./pages/Messagerie";
 import News from "./pages/News";
 import Mail from "./pages/Mail";
@@ -25,7 +24,6 @@ const App = () => {
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/benevoles" element={<Benevoles />} />
             <Route exact path="/calendrier" element={<Calendrier />} />
-            <Route exact path="/faq" element={<Faq />} />
             <Route exact path="/news" element={<News />} />
             <Route exact path="/sessions" element={<Sessions />} />
             <Route path="/messagerie" element={<Messagerie />} />
